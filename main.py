be8683810f56c1ad8dd86163a4350c0ea0b5f064
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-05 10:10:59 -0500

    changed camel case to snake case

--- main.py
+++ main.py
@@ -8,38 +8,38 @@ Tonight we'll be focusing on 3 categories:
     [3] Pop Culture
     ''')
 
-scienceQuestion = "Name the scientist who invented blood plasma: "
-scienceAnswer = "Charles Drew"
-sportsQuestion = "Name the first African American to play in Major League Baseball: "
-sportsAnswer = "Jackie Robinson"
-popQuestion = "Name the music icon referred to as the King of Pop: "
-popAnswer = "Michael Jackson"
+science_question = "Name the scientist who invented blood plasma: "
+science_answer = "Charles Drew"
+sports_question = "Name the first African American to play in Major League Baseball: "
+sports_answer = "Jackie Robinson"
+pop_question = "Name the music icon referred to as the King of Pop: "
+pop_answer = "Michael Jackson"
 
-userCategoryChoice = int(input("Please enter the number that corresponds to your category of choice: "))
+user_category_choice = int(input("Please enter the number that corresponds to your category of choice: "))
 
-if not (userCategoryChoice == 1 or userCategoryChoice == 2 or userCategoryChoice == 3):
+if not (user_category_choice == 1 or user_category_choice == 2 or user_category_choice == 3):
     print('That is not a valid option.')
-elif userCategoryChoice == 1:
+elif user_category_choice == 1:
     print(f"You've selected Sports. Here's your sports trivia question.")
-    sportsResponse = input(f"{sportsQuestion}")
-    print(f"Your answer is: {sportsResponse}")
-    if sportsResponse.lower() == sportsAnswer.lower():
+    sports_response = input(f"{sports_question}")
+    print(f"Your answer is: {sports_response}")
+    if sports_response.lower() == sports_answer.lower():
         print("That's correct!")
     else:
         print("I'm sorry. That's incorrect.")
-elif userCategoryChoice == 2:
+elif user_category_choice == 2:
     print(f"You've selected Science. Here's your science trivia question.")
-    scienceResponse = input(f"{scienceQuestion}")
-    print(f"Your answer is: {scienceResponse}")
-    if scienceResponse.lower() == scienceAnswer.lower():
+    science_response = input(f"{science_question}")
+    print(f"Your answer is: {science_response}")
+    if science_response.lower() == science_answer.lower():
         print("That's correct!")
     else:
         print("I'm sorry. That's incorrect.")
 else:
     print(f"You've selected Pop Culture. Here's your pop culture trivia question.")
-    popCultureResponse = input(f"{popQuestion}")
-    print(f"Your answer is: {popCultureResponse}")
-    if popCultureResponse.lower() == popAnswer.lower():
+    pop_culture_response = input(f"{pop_question}")
+    print(f"Your answer is: {pop_culture_response}")
+    if pop_culture_response.lower() == pop_answer.lower():
         print("That's correct!")
     else:
         print("I'm sorry. That's incorrect.")
